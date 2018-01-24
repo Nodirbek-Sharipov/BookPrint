0401fe50b93a167a8e6e4363c0f21cf21ed39b22
Nodirbek-Sharipov <33871499+Nodirbek-Sharipov@users.noreply.github.com>
2018-01-24 18:41:03 +0500

    Initial commit

---
+++ BookPrint/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.nudFnum = New System.Windows.Forms.NumericUpDown()
+        Me.nudLnum = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.rtbxFront = New System.Windows.Forms.RichTextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.rtbxBack = New System.Windows.Forms.RichTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblPages = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.nudFnum, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudLnum, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.nudFnum)
+        Me.GroupBox1.Controls.Add(Me.nudLnum)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Navy
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderSize = 3
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'nudFnum
+        '
+        resources.ApplyResources(Me.nudFnum, "nudFnum")
+        Me.nudFnum.Maximum = New Decimal(New Integer() {-1530494977, 232830, 0, 0})
+        Me.nudFnum.Name = "nudFnum"
+        '
+        'nudLnum
+        '
+        resources.ApplyResources(Me.nudLnum, "nudLnum")
+        Me.nudLnum.Maximum = New Decimal(New Integer() {-1530494977, 232830, 0, 0})
+        Me.nudLnum.Name = "nudLnum"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'rtbxFront
+        '
+        Me.rtbxFront.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        resources.ApplyResources(Me.rtbxFront, "rtbxFront")
+        Me.rtbxFront.Name = "rtbxFront"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'rtbxBack
+        '
+        Me.rtbxBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        resources.ApplyResources(Me.rtbxBack, "rtbxBack")
+        Me.rtbxBack.Name = "rtbxBack"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'lblPages
+        '
+        resources.ApplyResources(Me.lblPages, "lblPages")
+        Me.lblPages.BackColor = System.Drawing.Color.Transparent
+        Me.lblPages.Name = "lblPages"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label6.Name = "Label6"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblPages)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.rtbxBack)
+        Me.Controls.Add(Me.rtbxFront)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.nudFnum, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudLnum, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents nudFnum As NumericUpDown
+    Friend WithEvents nudLnum As NumericUpDown
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents rtbxFront As RichTextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents rtbxBack As RichTextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblPages As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ BookPrint/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim fnum As Integer = nudFnum.Text
+        Dim lnum As Integer = nudLnum.Text
+
+        If ((lnum - fnum + 1) Mod 4 = 0) Then
+            Dim entire(lnum - fnum) As Integer
+            Dim pages As Integer = (lnum - fnum + 1) / 4
+
+            Dim front As String = ""
+            Dim back As String = ""
+
+#Region " Entire array appending "
+            rtbxBack.Text = ""
+            rtbxFront.Text = ""
+
+            Dim incnum As Integer = 0
+            Dim sincnum As Integer = -1
+
+            For I1 As Integer = lnum To fnum Step -1
+
+                If (I1 Mod 2 = 0) Then
+                    If I1 < lnum Then
+                        incnum = incnum + 2
+                        entire(incnum) = I1
+                    Else
+                        entire(0) = lnum
+                    End If
+                End If
+            Next
+
+            For I2 As Integer = fnum To lnum
+                If (I2 Mod 2 <> 0) Then
+                    sincnum = sincnum + 2
+                    entire(sincnum) = I2
+                End If
+            Next
+#End Region
+
+            For lp As Integer = 0 To (entire.Length / 2 - 1)
+                front &= entire(lp)
+                If lp < (entire.Length / 2 - 1) Then
+                    front &= ","
+                End If
+            Next
+
+            For pl As Integer = (entire.Length / 2) To (entire.Length - 1)
+                back &= entire(pl)
+                If pl < (entire.Length - 1) Then
+                    back &= ","
+                End If
+            Next
+
+            rtbxFront.Text = front
+            rtbxBack.Text = back
+            lblPages.Text = pages
+
+        Else
+            MsgBox("Difference of two numbers must be divideble by 4" + vbNewLine + "Ex: From 1 till 40 => 40 numbers, divideble by 4")
+        End If
+
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        MessageBox.Show("This application was created by Nodirbek Sharipov (Student of WIUT 00005623) in order to simplify the process of printing" + vbNewLine + "Copyright 2017 © Nodirbek", "About", MessageBoxButtons.OK)
+    End Sub
+
+End Class
